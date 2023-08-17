9b65016a2980af5deb566303b7713335bbefdfda
ThanhLuan150 <116501650+ThanhLuan150@users.noreply.github.com>
2023-08-17 23:51:02 +0700

    Room

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\FoodController;
+use App\Http\Controllers\RoomController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +16,19 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
+//Rooms
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/category',[RoomController::class,'getCategoryRoom']);
+
+Route::get('/rooms',[RoomController::class ,'getRoom']);
+
+Route::get('/callroomservice',[RoomController::class ,'getcallRoomService']);
+
+Route::get('/callkitchen',[RoomController::class ,'getketchenRoomService']);
+
+Route::get('/room/{id_rooms}',[RoomController::class ,'getRoomDetail']);
+
+Route::get('/similar-rooms/{id_room}', [RoomController::class, 'getSimilarRooms']);
+Route::get('/type-rooms/{id_categori_room}', [RoomController::class, 'getTypeRooms']);
